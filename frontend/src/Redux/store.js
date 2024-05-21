3dc4b09c2ac340be80b3aef1b0b42d48dfd45733
ravikiranp04 <148761034+ravikiranp04@users.noreply.github.com>
2024-05-21 17:27:51 +0530

    Delete all

--- frontend/src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userLoginReducer from './slices/userLoginSlice'
-const reduxStore=configureStore({
-    reducer:{
-        userLogin:userLoginReducer
-    }
-})
-export default reduxStore;
